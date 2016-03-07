8e981eb1d9d96a39aa9cfe20e92729e98eba5153
Jeeva Kandasamy <jkandasa@gmail.com>
2016-03-07 13:15:42 +0530

    converting project as simple modules

--- dist/locale_fix.sh
+++ dist/locale_fix.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
-# Copyright (C) 2015-2016 Jeeva Kandasamy (jkandasa@gmail.com)
+# Copyright 2015-2016 Jeeva Kandasamy (jkandasa@gmail.com)
+# and other contributors as indicated by the @author tags.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#         http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
