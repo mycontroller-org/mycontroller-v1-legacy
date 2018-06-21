6dcdffe0db344d78e26f2677aa4e756c306589dc
Victória Moraes <victoria.moraes@aluno.ufms.br>
2018-06-21 19:52:10 -0400

    Update BaseCalendar.java

--- modules/core/src/main/java/org/mycontroller/standalone/calendar/BaseCalendar.java
+++ modules/core/src/main/java/org/mycontroller/standalone/calendar/BaseCalendar.java
@@ -1,28 +1,26 @@
+package org.mycontroller.standalone.calendar;
+
+import java.util.LinkedList;
 import java.util.List;
-import org.mycontroller.standalone.actor.Actor;
+
+import org.mycontroller.standalone.db.tables.Timer;
 
 public class BaseCalendar {
 
-	private static LinkedList<Calendar> calendars;
+	private LinkedList<Calendar> calendars;
 	
-	private static int calendarIndex;
-	private static Calendar calendar;
-	private static CalendarDay day;
-	private static Actor;
-	
-	BaseCalendar(Actor actor)
-	{
-		this.actor = actor;
-	}
+	private int calendarIndex;
+	private Calendar calendar;
+	private CalendarDay day;
 
-	public static int request(String request) {
+	public int request(String request) {
 		String[] tokens = request.split(" ");
 		switch(tokens[0])
 		{
 			case "newCalendar":
 				int index = calendars.size();
-				addCalendar(new Calendar(index));
-				actor.sendMessage("" + index);
+				addCalendar(new Calendar(Integer.toString(index)));
+				//return index to frontend
 				break;
 			case "openCalendar":
 				if(!getIndexByName(tokens[1]))
@@ -43,35 +41,35 @@ public class BaseCalendar {
 					calendar.setName(tokens[2]);
 				break;
 			case "addSensor":
-				int index = Integer.parseInt(tokens[1]);
+				int index1 = Integer.parseInt(tokens[1]);
 				//get sensor from index
 				calendar.addSensor(sensor);
 				break;
 			case "deleteSensor":
-				int index = Integer.parseInt(tokens[1]);
-				calendar.removeSensor(index);
+				int index1 = Integer.parseInt(tokens[1]);
+				calendar.removeSensor(index1);
 				break;
 			case "deleteActivation":
-				int index = Integer.parseInt(tokens[1]);
-				day.removeActivation(index);
+				int index1 = Integer.parseInt(tokens[1]);
+				day.removeActivation(index1);
 				break;
 			case "openCalendarDay":
 				//arrumar
-				long daytime = Long.parseInt(tokens[1]);
+				long daytime = Long.parseLong(tokens[1]);
 				day = calendar.getDay(daytime);
 				int activationSize = day.getActivationAmount();
-				actor.sendMessage("" + activationSize);
+				//return activationSize to frontend
 				for(int i = 0; i < activationSize; i++)
 				{
 					SensorActivation activation = day.getActivation(i);
-					actor.sendMessage(activation.getSensorName() + " " + activation.getActivationTime().getTime());
+					//return activation to frontend
 				}
 				break;
 		}
 		return 0;
 	}
 	
-	private static boolean getIndexByName(String name) {
+	private boolean getIndexByName(String name) {
 		for(int index=0;index < calendars.size();index++) {
 			if(calendars[index].getName() == tokens[1]) {
 				this.calendarIndex = index;
@@ -81,15 +79,15 @@ public class BaseCalendar {
 		return false;
 	}
 
-	public static void addCalendar(Calendar cal) {
+	public void addCalendar(Calendar cal) {
 		calendars.add(cal);
 	}
 
-	public static void removeCalendar(int index) {
+	public void removeCalendar(int index) {
 		calendars.remove(index);
 	}
 
-	public static void onTimerActivate(Timer timer) {
+	public void onTimerActivate(Timer timer) {
 		//timer activation call this function
 	}
 
